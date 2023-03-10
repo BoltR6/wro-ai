fb566ffd7e3bb2b9ac8f0b53431c981ed25e0273
BoltR6 <johnwallacewhite@gmail.com>
2023-03-09 20:09:53 -0600

    Basic classes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+pub struct Motor {
+    max_voltage: u8,
+    min_voltage: i8,
+    port: i8,
+}
+pub struct Bot {
+    drive_motor: Motor,
+    angle_motor: Motor,
+}
+impl Bot {
+    fn drive_forward(voltage_percent: i8){
+        assert!(voltage_percent < 100 && voltage_percent > 100, "Given percent oob!");
+
+        //drive motor
+    }
+    fn drive_angle(angle_degrees: u16){
+        assert!(angle_degrees > 0 && angle_degrees < 360, "Provided angle must be within 0 and 360");
+
+        //drive angle
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    
 }
